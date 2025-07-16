b4ef1103f33759a65feeec8d7b8858f6c687300c
vale0214 <vv.cevallosb@uea.edu.ec>
2025-07-15 19:36:06 -0500

    Update Program.cs

--- semana5/Program.cs
+++ semana5/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+class Program
+{
+    static void Main()
+    {
+        // Crear una lista de asignaturas
+        List<string> asignaturas = new List<string>()
+        {
+            "Inglés",
+            "Robótica",
+            "Química",
+            "Ciencias",
+            "Sociales"
+        };
+
+        // Mostrar las asignaturas por pantalla
+        Console.WriteLine("Asignaturas del curso:");
+        foreach (string asignatura in asignaturas)
+        {
+            Console.WriteLine("- " + asignatura);
+        }
+    }
+}
+
